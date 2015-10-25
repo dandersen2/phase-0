ade6ea91425c9d346a8c58ce130f398bdee22eda
Dan Andersen <dandersen2@gmail.com>
2015-10-25 18:13:29 -0400

    Add address and variable/method files

---
+++ week-4/variables-methods.rb
@@ -0,0 +1,18 @@
+#Full Name Greeting program:
+
+# puts "Please enter your first name"
+# first_name = gets.chomp
+# puts "Please enter your middle name"
+# middle_name = gets.chomp
+# puts "Please enter your last name"
+# last_name = gets.chomp
+
+# puts "Hello " + first_name + ' ' + middle_name + ' ' + last_name + '!'
+
+#Better favorite number program:
+
+puts "What is your favorite number?"
+fav = gets.chomp
+new_fav = fav.to_i + 1
+puts "Are you sure you wouldn't prefer " + new_fav.to_s + " as your favorite number?"
+
