d5ce31b5d3e1d693df6711dab745d24e0788a75e
Dan Andersen <dandersen2@gmail.com>
2015-10-25 22:48:55 -0400

    Add good-guess and letter-grade files

---
+++ week-4/good-guess/my_solution.rb
@@ -0,0 +1,16 @@
+# Good Guess
+
+# I worked on this challenge by myself.
+
+
+# Your Solution Below
+
+def good_guess?(integer)
+#   puts "Please enter a number."
+#   integer = gets.chomp
+   if integer.to_i == 42
+      return true
+   else
+      return false
+   end
+end
