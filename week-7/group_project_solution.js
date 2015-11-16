e4b4a9c9f04cf530f9008e30218b0812cfd0a7bd
Dan Andersen <dandersen2@gmail.com>
2015-11-16 11:26:03 -0500

    Update group project solution file to most recent version

--- week-7/group_project_solution.js
+++ week-7/group_project_solution.js
@@ -111,6 +111,7 @@ var array = [1,2,3,4,5,6,7,8,9,10,11]
 
 //Return the value of arraySum (i.e., the sum of all the integers in the
 //input array).
+
 var Sum = function (array){
   var arraySum = array.reduce((a, b) => a + b);
   return arraySum;
@@ -122,7 +123,7 @@ console.log(Sum(array)); //driver code for me
 // Input: an array of integers
 // Output: an integer (the sum of all the integers in the input array,
 // divided by the number of integers in the array)
-var integers = [1,2,3,5,6,7,8,9,10,11,12,13];
+ var integers = [1,2,3,5,6,7,8,9,10,11,12,13];
 // Steps:
 // Create a function called Mean that accepts an array of integers as its
 // input.
@@ -161,7 +162,7 @@ var Mean = function(integers){
 };
 Mean(integers);
 
-// Function 3: Median
+// Function 3: Median (Syema Ailia)
 // Input: an array of integers
 // Output: an integer (see steps below for clarification of what this
 // integer is)
@@ -183,6 +184,19 @@ Mean(integers);
 //   oddMedianIndex's position.
 //   Return the integer located at oddMedianIndex's position.
 //   END.
+
+var array = [1,2,3,5,6,7,8,9,10,11,12]
+
+var Median = function(array) {
+  var integerLength = array.length;
+  if (integerLength % 2 !== 0)  {
+  var odd_num = integerLength / 2;
+  oddMedianIndex = Math.ceil(odd_num);
+  return array[oddMedianIndex]
+}
+}
+console.log(Median(array));
+
 // If the input array has an even number of integers:
 //   divide the number of integers in the array by 2.
 //   Set evenMedianIndex1 equal to the result of the previous step.
@@ -201,3 +215,91 @@ Mean(integers);
 //   Set evenMedianOutput equal to the result of this division.
 //   Return evenMedianOutput.
 //   END.
+var Median = function(array) {
+  var integerLength = array.length;
+  if (integerLength % 2 == 0) {
+  var evenMedianIndex1 = integerLength / 2;
+  var evenMedianIndex2 = (evenMedianIndex1 / 2) + 1;
+  var evenMedianOutput = (array[evenMedianIndex1] + array[evenMedianIndex2]) / 2;
+  return evenMedianOutput;
+}
+}
+console.log(Median(array));
+
+
+
+
+
+
+/*
+  Release 4 part 1: Refactor
+  Person 5: Refactor (by Bernice Anne W. Chua)
+*/
+var sum = function(integers_array){
+  return integers_array.reduce((a, b) => a + b);
+};
+console.log("Sum of [1, 2, 3, 4]: " + sum([1, 2, 3, 4]));
+console.log("Sum of [1, 2, 3, 4, 5]: " + sum([1, 2, 3, 4, 5]));
+console.log("Sum of integers = [1,2,3,5,6,7,8,9,10,11,12,13]: " + sum(integers));
+
+var mean = function(integers_array){
+  return Sum(integers_array) / integers_array.length;
+};
+console.log("Mean of [1, 2, 3, 4]: " + mean([1, 2, 3, 4]));
+console.log("Mean of [1, 2, 3, 4, 5]: " + mean([1, 2, 3, 4, 5]));
+console.log("Mean of integers = [1,2,3,5,6,7,8,9,10,11,12,13]: " + mean(integers));
+
+var median = function(numbersArray) {
+  var sortedNumbers = numbersArray.sort();  // Numbers need to be in order to find the median.
+  var middleIndexOfNumbers = sortedNumbers.length / 2;
+  if (sortedNumbers.length % 2 === 0) {
+    return (sortedNumbers[(middleIndexOfNumbers) - 1] + sortedNumbers[middleIndexOfNumbers]) / 2.0;
+    // I put "- 1" because the index of an array starts at zero.
+  } else {
+    return sortedNumbers[Math.floor(sortedNumbers.length / 2)];
+    // I put "- 1" because the index of an array starts at zero.
+  }
+};
+console.log("Median of [1, 2, 3, 4]: " + median([1, 2, 3, 4]));
+console.log("Median of [1, 2, 3, 4, 5]: " + median([1, 2, 3, 4, 5]));
+console.log("Median of integers = [1,2,3,5,6,7,8,9,10,11,12,13]: ");
+console.log(median([1,2,3,5,6,7,8,9,10,11,12,13]));
+console.log("Median of array = [1,2,3,5,6,7,8,9,10,11,12]: ");
+console.log(median([1,2,3,5,6,7,8,9,10,11,12]));
+
+___________________________________________________________________________________
+
+// Release 4 - Part 2: Translate to User stories
+// Person 6: Dan Andersen
+
+
+
+// Function 1: var sum = function(integers_array) {
+
+// As a user, I want to take a list of numbers and enter them into this function.
+// The function needs to take all of the numbers and add them together. Once the
+// total is calculated, the function should return a readable sentence that lists
+// the original list of numbers and states what their summed total is.
+//     eg: Sum of [1, 2, 3, 4]: 10
+
+
+
+// Function 2: var mean = function(integers_array){
+
+// As a user, I want to take a list of numbers and use the sum function on them
+// to calculate the total. Then I want to divide that number by the number of values
+// in my original list. When I run my mean function on a list of numbers I want to
+// recieve a printout with the list of numbers and a statement including what the
+// mean of those numbers is.
+//     eg: Mean of [1, 2, 3, 4]: 2.5
+
+
+
+// Function 3: var median = function(numbersArray) {
+
+// As a user, I want to take a list of numbers and when I run the median function
+// on them, the numbers must be put in order from smallest to largest. Next, the
+// function must locate the middle number and print out that number as the median.
+// If there are an even number of values, the function must take the average of
+// the two middle-most numbers and return that as the median.
+//     eg: Median of [1, 2, 3, 4]: 2.5
