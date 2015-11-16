441828bc904c3257cb7724f61d7b2e9dd40090f7
Dan Andersen <dandersen2@gmail.com>
2015-11-16 10:55:19 -0500

    Add reflections to nums_commas solution file

--- week-7/nums_commas.js
+++ week-7/nums_commas.js
@@ -75,14 +75,32 @@ console.log( separateComma( 4192482489027 ) );
 
 // Reflection
 
-QUESTION: What was it like to approach the problem from the perspective of JavaScript? Did you approach the problem differently?
-  ANSWER: It was interesting to pseudocode with the goal of coding in another language in mind because I noticed which words and concepts still apply sort of universally to both Ruby and Javascript but are still specific enough to be helpful. I approached the problem in the same general way though, creating functions instead of methods, making local variables, and iterating over data structures.
+// QUESTION: What was it like to approach the problem from the perspective
+// of JavaScript? Did you approach the problem differently?
+//   ANSWER: It was interesting to pseudocode with the goal of coding in
+//   another language in mind because I noticed which words and concepts
+//   still apply sort of universally to both Ruby and Javascript but are
+//   still specific enough to be helpful. I approached the problem in the
+//   same general way though, creating functions instead of methods, making
+//   local variables, and iterating over data structures.
 
-QUESTION: What did you learn about iterating over arrays in JavaScript?
-  ANSWER:
+// QUESTION: What did you learn about iterating over arrays in JavaScript?
+//   ANSWER: I found that the for loop is very efficient and easy to
+//   setup and use compared to incrementing while or until loops in
+//   Ruby. Also many of the methods seem to be the same or very similar
+//   to Ruby such as .split, .reverse, and .join.
 
-QUESTION: What was different about solving this problem in JavaScript?
-  ANSWER:
+// QUESTION: What was different about solving this problem in JavaScript?
+//   ANSWER: The syntax for js was the part that seemed the most different
+//   from Ruby, such as using the {} brackes for the loop like we would
+//   for a block in Ruby, ending each statement with a semicolon ;, and
+//     using console.log() with the function as an argument were some
+//   slight differences. Buidling a function was ver similar to creating
+//   methods in Ruby, so the overal structure was familiar.
 
-QUESTION: What built-in methods did you find to incorporate in your refactored solution?
-  ANSWER:
+// QUESTION: What built-in methods did you find to incorporate in your
+// refactored solution?
+//   ANSWER: We used. split to separate a string into an array, .reverse
+//   to reverse the order of the array, and .join to put the array back
+//   together into a single string. We also used the .splice to insert
+//   the commawhere it was needed. this was similar to inject in Ruby.
